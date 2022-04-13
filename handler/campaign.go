bea73dedc8eb276f90d78dec563c7252c82953d2
Febriandani <febriandani176@gmail.com>
2022-04-13 15:38:18 +0700

    Campaign repository

---
+++ handler/campaign.go
@@ -0,0 +1,8 @@
+package handler
+
+// tangkap parameter di handler
+// handler ke service
+// service menentukan apakah repository mana yang di call
+// repository : FindAll, FindByUserID
+// db
+
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golang-startup-web/auth"
+	"golang-startup-web/campaign"
 	"golang-startup-web/handler"
 	"golang-startup-web/helper"
 	"golang-startup-web/user"
@@ -25,6 +26,21 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(1)
+
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println(len(campaigns))
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+		fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 	if err != nil {
