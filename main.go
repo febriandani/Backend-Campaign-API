c2725ac93e58ea251afc64a9fe74a9a2815eef07
Febriandani <febriandani176@gmail.com>
2022-04-14 14:55:00 +0700

    campaign service, GetCampaigns

--- main.go
+++ main.go
@@ -28,26 +28,14 @@ func main() {
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 
-	campaigns, err := campaignRepository.FindByUserID(1)
-
-	fmt.Println("debug")
-	fmt.Println("debug")
-	fmt.Println("debug")
-	fmt.Println(len(campaigns))
-	for _, campaign := range campaigns {
-		fmt.Println(campaign.Name)
-		if len(campaign.CampaignImages) > 0 {
-		fmt.Println(campaign.CampaignImages[0].FileName)
-		}
-	}
-
 	userService := user.NewService(userRepository)
+	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
-	if err != nil {
-		fmt.Println("ERROR")
-		fmt.Println("ERROR")
-		fmt.Println("ERROR")
-	}
+
+	campaigns, _ := campaignService.FindCampaigns(9)
+		fmt.Println(len(campaigns))
+
+	
 	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
