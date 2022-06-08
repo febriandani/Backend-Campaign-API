83cf97ea71724d70a53fe4a39b5cb41425d3fd1b
magang febri <febriandani176@gmail.com>
2022-06-08 09:53:45 +0700

    Update authMiddleware

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"golang-startup-web/auth"
 	"golang-startup-web/handler"
+	"golang-startup-web/helper"
 	"golang-startup-web/user"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -30,7 +34,54 @@ func main() {
 	api.POST("/regusers", userHandler.RegisterUser)
 	api.POST("/login", userHandler.Login)
 	api.GET("/email_check", userHandler.CheckEmailAvailable)
-	api.POST("/upload_av", userHandler.UploadAvatar)
+	api.POST("/upload_av", authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 	route.Run()
+}
+
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		//cek jika didalam authHeader tidak ada Bearer
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIresponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		tokenString := ""
+		//split menggabungkan Bearer spasi tokentokentoken
+		arrayToken := strings.Split(authHeader, " ")
+		if len(tokenString) == 2 {
+			tokenString = arrayToken[1]
+		}
+
+		token, err := authService.ValidateToken(tokenString)
+		if err != nil {
+			response := helper.APIresponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claim, ok := token.Claims.(jwt.MapClaims)
+
+		if !ok || !token.Valid {
+			response := helper.APIresponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		userID := int(claim["user_id"].(float64))
+
+		user, err := userService.GetUserByID(userID)
+		if err != nil {
+			response := helper.APIresponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		c.Set("currentUser", user)
+	}
+
 }
